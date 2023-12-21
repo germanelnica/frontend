90c447e24b3d57e8a88c98267dee9196807b687d
germanbegone <german.nunez@ahinko.com>
2023-12-20 22:26:23 -0600

    feature: remove unused comments

--- tasklist/src/App.tsx
+++ tasklist/src/App.tsx
@@ -1,16 +1,15 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Menu from "./components/menu"; // El componente que contiene los botones
-import ListTask from "./components/list"; // El componente de las tareas
+import ListTask from "./components/list"; // El componente de las listado
 import Tasks from "./components/tasks/tasks"; // El componente de las tareas
 import "antd/dist/antd.css";
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Menu />} /> {/* 👈 Renders at /app/ */}
-			<Route path="/tasks" element={<Tasks />} /> {/* 👈 Renders at /app/ */}
-			<Route path="/list" element={<ListTask />} /> {/* 👈 Renders at /app/ */}
+			<Route path="/" element={<Menu />} />
+			<Route path="/tasks" element={<Tasks />} />
+			<Route path="/list" element={<ListTask />} />
 		</Routes>
 	);
 }
